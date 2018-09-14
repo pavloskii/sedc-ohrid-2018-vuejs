b609a4b92dcd6fae3d571e0bd3319a2e6416947e
Igor Pavloski <Igor.Pavloski@seavus.com>
2018-09-14 19:00:38 +0200

    First Vuejs project with Vue-cli: Components and comp communication

---
+++ first-vuejs-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <h1>{{title}}</h1>
+    <p>This is our first Vuejs app</p>
+    <button @click="changeTitle">Change Title</button>
+
+    <hr>
+
+    <app-user-details v-bind:desc="description"></app-user-details>
+
+  </div>
+</template>
+
+<script>
+import UserDetails from './components/User/UserDetails';
+
+export default {
+  data() {
+    return {
+      title: "Hello World",
+      description: "Learnig Vuejs is fun",
+    };
+  },
+  methods: {
+    changeTitle(){
+      this.title = "Ok we are done!"
+    }
+  },
+  components:{
+    "app-user-details": UserDetails
+  }
+};
+</script>
+
+<style scoped>
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.5);
+    padding: 15px;
+  }
+</style>
---
+++ first-vuejs-app/src/components/User/UserDetails.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <p>{{lastName}}</p>
+        <p>{{desc}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Igor",
+      lastName: "Pavloski",
+      description: "Learning VueJS"
+    };
+  },
+  props: {
+    desc: {
+        type: String,
+        // required: true,
+        default: "This is the default description"
+    }
+  }
+};
+</script>
