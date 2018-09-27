3d98acba5e23212d042be41c1999548bdab88c51
Igor Pavloski <igor.pavloski@seavus.com>
2018-09-27 19:36:26 +0200

    ajde ova e za marija

--- login-vuex-project/src/components/Students.vue
+++ login-vuex-project/src/components/Students.vue
@@ -1,17 +1,38 @@
 <template>
     <div>
         <h1>Students in SEDC:</h1>
+        <hr>
+        <div class="row">
+            <div 
+                v-for="(student, index) in students" 
+                class="card col-md-3" 
+                style="width: 18rem;"
+                :key="index"
+            >
+                <img class="card-img-top" :src="student.image" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{student.firstname}} {{student.lastname}}</h5>
+                    <p class="card-text">{{student.description}}</p>
+                    <p>Birthday: {{student.birthday}}</p>
+                </div>
+            </div>
+        </div>
+       
     </div>
 </template>
 
 <script>
 export default {
-    created(){
-        this.$store.dispatch("getStudents");
+  computed: {
+    students() {
+      return this.$store.getters.getStudents;
     }
-}
+  },
+  created() {
+    this.$store.dispatch("getStudents");
+  }
+};
 </script>
 
 <style>
-
 </style>
--- login-vuex-project/src/store/index.js
+++ login-vuex-project/src/store/index.js
@@ -12,8 +12,10 @@ export default new Vuex.Store({
     getters: {
         isUserAuthenticated(state) {
             return state.loggedUser !== null;
+        },
+        getStudents(state){
+            return state.students;
         }
-
     },
     mutations: {
         setUser(state, payload) {
